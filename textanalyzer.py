c2216f454d50310384e70ea32859dc9061dcfd4f
qvu3 <75177814+qvu3@users.noreply.github.com>
2022-02-09 23:22:35 -0500

    Submit the Text Analyzer assignment
    This is the solution for the poem text frequencies analyzer assignment.

---
+++ textanalyzer.py
@@ -0,0 +1,14 @@
+from collections import Counter
+
+textHolder = []
+
+with open(r"C:\Users\VQHSon\Desktop\Poem.txt", encoding="utf8") as file:
+    reader = file.read()
+    word_split = reader.split(" ")
+    for w in word_split:
+        textHolder.append(w)
+file.close()
+
+counter = Counter(textHolder)
+
+print(dict(counter.most_common(5)))
